cae996812a1e960c413f83a75f8110be097b98b1
unknown <steven.wright@US-SLS-StevenW.red-gate.com>
2015-07-30 12:28:25 -0700

    Modified getcontacts sproc and added a contact type

--- Stored Procedures/dbo.usp_GetContacts.sql
+++ Stored Procedures/dbo.usp_GetContacts.sql
@@ -1,10 +1,14 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-CREATE PROCEDURE [dbo].[usp_GetContacts] AS
-BEGIN
-SELECT * FROM Person.Person
-END
+CREATE PROCEDURE [dbo].[usp_GetContacts]
+AS
+    BEGIN
+        SELECT  FirstName ,
+                LastName
+        FROM    Person.Person;
+    END;
 
 GO
