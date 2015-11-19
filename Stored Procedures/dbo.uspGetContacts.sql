4a1eff816efe4af1d9fa33140cfd2f3de0119dc3
unknown <steven.wright@RED-GATE>
2015-11-19 12:22:35 -0800

    Modified getcontacts sproc

--- Stored Procedures/dbo.uspGetContacts.sql
+++ Stored Procedures/dbo.uspGetContacts.sql
@@ -1,10 +1,15 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-CREATE PROCEDURE [dbo].[uspGetContacts] AS
-BEGIN
-SELECT * FROM Person.Person
-END
+CREATE PROCEDURE [dbo].[uspGetContacts]
+AS
+    BEGIN
+        SELECT  FirstName ,
+                LastName ,
+                MiddleName
+        FROM    Person.Person;
+    END;
 
 GO
